501ea0232ec42e64907c4f133fb96f743effa37e
Kristian Wrona <kwrona@smail.uni-koeln.de>
2021-11-02 00:06:01 +0100

    add elevator program

---
+++ Elevator.java
@@ -0,0 +1,48 @@
+public class Elevator{
+	
+	public static int current_Floor=5;
+	int dest_Floor;
+	
+	private Elv_States State = new Elv_States();
+	
+	public Elevator (int dest_floor) 
+	{
+		this.dest_Floor=dest_floor;
+	if(dest_floor<0|| dest_floor>10) {
+		System.out.println("Wrong floor9");
+	}
+	else {
+		
+	
+	
+	System.out.println("*** Destination floor is: "+ dest_Floor);
+	if(dest_floor>current_Floor) {
+		while(current_Floor<dest_floor) {
+			System.out.println("*** "+State.Moving_up);
+			current_Floor++;
+			System.out.println("*** Current Floor "+current_Floor);
+		}
+		
+	}
+	else if (dest_floor<current_Floor) {
+		while(current_Floor>dest_floor) {
+			System.out.println("*** "+State.Moving_down);
+			current_Floor--;
+			
+			System.out.println("*** Current Floor "+current_Floor);
+		}
+		
+	}
+	arrive_atFloor();	
+	}
+	
+
+
+	}
+
+	private void arrive_atFloor() {
+		System.out.println("*** You have reached the: "+ dest_Floor+"th floor");
+	}
+
+	
+}
---
+++ Elv_States.java
@@ -0,0 +1,6 @@
+public class Elv_States {
+	
+	public  String Moving_up = "Moving_up";
+	public  String Moving_down = "Moving_down";
+
+}
